dea569c40564ef0ed4e51486730b921aa7dbd38f
LoLlYdE <11347524+LoLlYdE@users.noreply.github.com>
2018-08-31 20:44:44 +0200

    Update hello_name.js

--- src/lambda/hello_name.js
+++ src/lambda/hello_name.js
@@ -9,7 +9,7 @@ exports.handler = async (event, context) => {
     .then(response => response.json())
     .then(data => ({
       statusCode: 200,
-      body: `${JSON.stringify(data)}`
+      body: `url: ${API_ENDPOINT + id} response: ${JSON.stringify(data)}`
     }))
     .catch(error => ({ statusCode: 422, body: String(error) }));
 };
