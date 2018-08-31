648979c035308673dcebca3a4f516d355951efb8
LoLlYdE <11347524+LoLlYdE@users.noreply.github.com>
2018-08-31 20:12:26 +0200

    Update hello_name.js

--- src/lambda/hello_name.js
+++ src/lambda/hello_name.js
@@ -4,7 +4,7 @@ const API_ENDPOINT =
   "http://store.steampowered.com/api/appdetails?appids=";
 
 exports.handler = async (event, context) => {
-  const passed_id = event.queryStringParameters.id
+  var passed_id = event.queryStringParameters.id
   return fetch(API_ENDPOINT + passed_id)
     .then(response => response.json())
     .then(data => ({
