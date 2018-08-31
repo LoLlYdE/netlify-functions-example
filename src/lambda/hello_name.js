cd798ad1f28514c5d03ff006d27446f668e41a8a
LoLlYdE <11347524+LoLlYdE@users.noreply.github.com>
2018-08-31 20:10:18 +0200

    Update hello_name.js

--- src/lambda/hello_name.js
+++ src/lambda/hello_name.js
@@ -1,8 +1,15 @@
-exports.handler = async (event, context) => {
-  const name = event.queryStringParameters.name || "World";
+import fetch from "node-fetch";
 
-  return {
-    statusCode: 200,
-    body: `Hello, ${name}`
-  };
+const passed_id = event.queryStringParameters.id
+const API_ENDPOINT =
+  "http://store.steampowered.com/api/appdetails?appids=" + passed_id;
+
+exports.handler = async (event, context) => {
+  return fetch(API_ENDPOINT)
+    .then(response => response.json())
+    .then(data => ({
+      statusCode: 200,
+      body: `${data.passed_id.data.name}`
+    }))
+    .catch(error => ({ statusCode: 422, body: String(error) }));
 };
