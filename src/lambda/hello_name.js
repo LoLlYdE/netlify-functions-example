836c46457641486dd3f7c7717c9432980d14bdbe
LoLlYdE <11347524+LoLlYdE@users.noreply.github.com>
2018-08-31 20:11:31 +0200

    Update hello_name.js

--- src/lambda/hello_name.js
+++ src/lambda/hello_name.js
@@ -1,11 +1,11 @@
 import fetch from "node-fetch";
 
-const passed_id = event.queryStringParameters.id
 const API_ENDPOINT =
-  "http://store.steampowered.com/api/appdetails?appids=" + passed_id;
+  "http://store.steampowered.com/api/appdetails?appids=";
 
 exports.handler = async (event, context) => {
-  return fetch(API_ENDPOINT)
+  const passed_id = event.queryStringParameters.id
+  return fetch(API_ENDPOINT + passed_id)
     .then(response => response.json())
     .then(data => ({
       statusCode: 200,
